6c71fd094216e95411a7894348eb36ddef7f1a36
Robert Gripshover <robertgrips@gmail.com>
2022-07-18 13:33:10 -0400

    this is one of my first commits redoing this sprint, i am adding work to the tabs.js file

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -12,7 +14,40 @@ const Tabs = (topics) => {
   //   <div class="tab">bootstrap</div>
   //   <div class="tab">technology</div>
   // </div>
-  //
+  
+
+  const topicsDiv = document.createElement('div');
+  const javascriptTab = document.createElement('div');
+  const bootstrapTab = document.createElement('div');
+  const technologyTab = document.createElement('div');
+  const jqueryTab = document.createElement('div');
+  const nodeTab = document.createElement('div');
+
+  topicsDiv.appendChild(javascriptTab);
+  topicsDiv.appendChild(bootstrapTab);
+  topicsDiv.appendChild(technologyTab);
+  topicsDiv.appendChild(jqueryTab);
+  topicsDiv.appendChild(nodeTab);
+
+  topicsDiv.classList.add('topics');
+  javascriptTab.classList.add('tab');
+  bootstrapTab.classList.add('tab');
+  technologyTab.classList.add('tab');
+  jqueryTab.classList.add('tab');
+  nodeTab.classList.add('tab');
+
+//For this for loop below VVV I might want to try and turn the topics info into an Array
+// like we were doing last week in class, and then forEach over it.
+
+  for (let i=0; i < topics.length; i++){
+    javascriptTab.textContent = topics[i];
+    bootstrapTab.textContent = topics[i];
+    technologyTab.textContent = topics[i];
+    jqueryTab.textContent = topics[i];
+    nodeTab.textContent = topics[i];
+  }
+
+  return topicsDiv;
 }
 
 const tabsAppender = (selector) => {
